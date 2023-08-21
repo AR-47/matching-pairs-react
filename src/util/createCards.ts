9dfa7deddbada111f4da3e3896f3eb3408281d81
Adil Rahman <arahman.99@outlook.com>
2023-08-21 13:29:58 +0100

    refactored the createCards function

--- src/util/createCards.ts
+++ src/util/createCards.ts
@@ -1,21 +1,10 @@
-/* Randomize array in-place using Durstenfeld shuffle algorithm */
-// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffleArray(array: string[]) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
-}
+import { shuffleArray } from "./shuffleArray";
 
 export const createCards = (cardList: string[]): string[] => {
     const chosenCardsSet = new Set<string>();
     while (chosenCardsSet.size !== 8) {
-        chosenCardsSet.add(
-            cardList[Math.floor(Math.random() * cardList.length)]
-        );
+        const randomIdx = Math.floor(Math.random() * cardList.length);
+        chosenCardsSet.add(cardList[randomIdx]);
     }
     const chosenCardsList = shuffleArray([
         ...chosenCardsSet,
@@ -23,5 +12,3 @@ export const createCards = (cardList: string[]): string[] => {
     ]);
     return chosenCardsList;
 };
-
-console.log(shuffleArray(["1", "2", "3"]));
